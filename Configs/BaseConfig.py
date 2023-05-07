2e44c51ffbf5befc387794145faf9787f09b0eaa
TOMphong <93088029+TOMphong@users.noreply.github.com>
2023-05-07 22:21:57 +0700

    add config folder

---
+++ Configs/BaseConfig.py
@@ -0,0 +1,9 @@
+class BaseConfig():
+    #TRANSFORMER
+    def __init__(self):
+        #DATA
+        self.DATA.VOCAB.NAME = "default"
+         
+
+        self.DATA.DATASET = ""
+        self.DATA.DATASET
